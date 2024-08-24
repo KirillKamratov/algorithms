bb296bba1727756f407b2d3f64cd81644890f1c1
Kirill Kamratov <kirill.kamratov@yandex.ru>
2024-08-24 16:56:47 +0300

    fix sorting algorithms

--- sorting/insertionSorting.js
+++ sorting/insertionSorting.js
@@ -1,14 +1,12 @@
 const insertionSorting = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < arr[i - 1]) {
-      for (let j = 0; j < i; j++) {
-        if (arr[j] > arr[i]) {
-          let temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
-        }
-      }
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = temp;
   }
   return arr;
 };
--- sorting/selectionSorting.js
+++ sorting/selectionSorting.js
@@ -1,15 +1,16 @@
 const selectionSorting = (arr) => {
   for (let i = 0; i < arr.length; i++) {
+    let min = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+      if (arr[min] > arr[j]) {
+        min = j;
       }
     }
-
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
   }
-  return arr
+  return arr;
 };
 
 console.log(selectionSorting([6, 5, 3, 1, 8, 6, 7, 2, 4]))
